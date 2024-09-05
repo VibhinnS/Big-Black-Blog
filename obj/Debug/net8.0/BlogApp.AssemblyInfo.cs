89bc306b23eba05a3cd7cdb1ef4da68778e8e6b3
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-05 21:24:50 +0530

    Updated Build

--- obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08e9fce709ce7c3e8dd4e07b4e1d959d53f0622")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
